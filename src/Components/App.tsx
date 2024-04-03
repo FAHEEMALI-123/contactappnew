d678dda300a9dd257cd2966a351692f9316780ce
FAHEEMALI-123 <faheemali370@gmail.com>
2024-04-03 21:53:26 +0300

    Add member commit

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -7,24 +7,42 @@ import EnterDet from "./EnterDet.tsx";
 
 function App() {
   const [employeedet, setEmployeedet] = useState<IEmployee[]>(dummyEmployee);
+  const[viewpage,SetViewpage]=useState<String>("table");
+
+
 
   const AddMember=()=>{
-    return(
-      <div>
-        <EnterDet />
-      </div>
-    );
+    SetViewpage("form");
+  }
+  const ShowListpage=()=>{
+    SetViewpage("table");
+  }
+
+  const AddEmployee=(data:IEmployee)=>{
+    setEmployeedet([...employeedet,data]);
+    console.log(setEmployeedet);
   }
 
   return (
     <div>
+      {
+      viewpage==="table" ?
+      (
+        <div>
       <div className="navbar bg-primary text-primary-content">
-        <button className="btn btn-ghost text-xl">ContactAPP</button>
-        <button className="btn btn-active btn-secondary" onClick={AddMember}>Add Member</button>
+      <button className="btn btn-ghost text-xl">ContactAPP</button>
+      <button className="btn btn-active btn-secondary" onClick={AddMember}>Add Member</button>
+      
       </div>
       <Tabler details={dummyEmployee} />
+      </div>
+     
+    ):
+    (<EnterDet onsubmitcli={AddEmployee}/>)
+       }
     </div>
   );
-}
+    }
+
 
 export default App;
--- src/Components/EnterDet.tsx
+++ src/Components/EnterDet.tsx
@@ -1,15 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import "./tabler.tsx";
+import { IEmployee } from "./Employeetype.ts";
+import './EnterDet.css';
 
-const EnterDet=()=>{
-    return(
-        <div>
-            <form>
-            <input type="text" placeholder="Name" className="input input-bordered input-info w-full max-w-xs" />
-            <input type="text" placeholder="Email" className="input input-bordered input-info w-full max-w-xs" />
-            <input type="text" placeholder="Phone" className="input input-bordered input-info w-full max-w-xs" />
-            </form>
-        </div>
-    );
+type props={
+    onsubmitcli:(data:IEmployee)=>void
+    
 }
 
+const EnterDet = (props: props) => {
+  const [name, SetName] = useState("");
+  const [email, SetEmail] = useState("");
+  const [phone, SetPhone] = useState(0);
+
+  const {onsubmitcli}=props;
+
+  const onchangename=(e:any)=>{
+    SetName(e.target.value);
+  }
+
+  const onchangeemail=(e:any)=>{
+    SetEmail(e.target.value);
+    console.log(email);
+  }
+  const onchangephone=(e:any)=>{
+    SetPhone(e.target.value);
+  }
+
+  const onsubmitbutton=(e:any)=>{
+    e.preventDefault();
+    const data:IEmployee={
+        id:new Date().toJSON.toString(),
+        name:name,
+        email:email,
+        phone:phone,
+    };
+    onsubmitcli(data);
+  };
+
+  return (
+    <div>
+      <div className="navbar bg-neutral text-neutral-content">
+        <button className="btn btn-ghost text-xl">Add Member</button>
+      </div>
+      <form onSubmit={onsubmitbutton}>
+      <label className="input input-bordered flex items-center gap-2">
+        <input
+          type="text"
+          value={name}
+          className="grow"
+          placeholder="Username"
+          onChange={onchangename}
+        />
+      </label>
+      <label className="input input-bordered flex items-center gap-2">
+        <input type="text" onChange={onchangeemail} value={email} className="grow" placeholder="Email" />
+      </label>
+
+      <label className="input input-bordered flex items-center gap-2">
+        <input
+          type="number"
+          value={phone}
+          className="grow"
+          placeholder="Phonenumber"
+          onChange={onchangephone}
+        />
+      </label>
+
+      <div className="butt">
+        <button className="btn btn-active btn-secondary ">View List</button>
+        <input type="submit" className="btn btn-primary text-xl" value="Add Employee"/>
+      </div>
+      </form>
+    </div>
+  );
+};
 export default EnterDet;
